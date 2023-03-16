47797484e3004bea329e81b9763a51eef847482c
Eleonora Kanybekova <113588011+elya2405@users.noreply.github.com>
2023-03-16 14:52:09 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import RedButton from "./RedButton";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <RedButton>Children</RedButton>
+    </div>
+  );
+}
+export default App;
